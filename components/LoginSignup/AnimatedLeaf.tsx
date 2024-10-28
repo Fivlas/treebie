155ec94612c4895af3b70fecca6f32fd71bfcfbf
TymianekPL <wilczew.tymon@gmail.com>
2024-10-28 21:42:33 +0100

    add [incomplete] login page (note: remove it as a tab and make automatic redirects)

---
+++ components/LoginSignup/AnimatedLeaf.tsx
@@ -0,0 +1,9 @@
+﻿import {View} from "react-native";
+
+export type AnimatedLeafOptions = {
+    className: string | undefined;
+};
+
+export function AnimatedLeaf({className}: Readonly<AnimatedLeafOptions>) {
+    return <View className={`rounded-full border-200  border-solid ${className}`}></View>
+}
