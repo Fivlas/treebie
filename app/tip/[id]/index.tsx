3964afc74115e672bc0b9c3e52716f6714af002e
Fivlas <filip.skoczylas777007@gmail.com>
2024-10-29 21:05:43 +0100

    adds firebase not completed

--- app/tip/[id]/index.tsx
+++ app/tip/[id]/index.tsx
@@ -1,14 +1,44 @@
 import CustomButton from "@/components/elements/CustomButton";
 import { router, useLocalSearchParams } from "expo-router";
-import { SafeAreaView, Text, View } from "react-native";
+import { ActivityIndicator, SafeAreaView, Text, View } from "react-native";
+import firestore from "@react-native-firebase/firestore";
+import { useEffect, useState } from "react";
 
 const index = () => {
+    const [data, setData] = useState(null);  // Stores fetched data
+    const [loading, setLoading] = useState(true);  // Tracks loading state
     const local = useLocalSearchParams();
+
+    const fetchData = async () => {
+        try {
+            const tip = await firestore()
+                .collection('tips')
+                .get();
+            console.log(tip);
+            // setData(tip);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        } finally {
+            setLoading(false);  // End loading
+        }
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    // dn28jsvwmtKdXJxzCNH9
+
 
     return (
         <SafeAreaView>
-            <Text>{local.id}</Text>
-            <CustomButton title="Back" handlePress={() => router.push("/(tabs)/")} buttonType="primary"/>
+            {/* @ts-ignore */}
+            {loading ? <ActivityIndicator size="large" color="#0000ff" /> : <Text>{local.id}</Text>}
+            <CustomButton
+                title="Back"
+                handlePress={() => router.push("/(tabs)/")}
+                buttonType="primary"
+            />
         </SafeAreaView>
     );
 };
