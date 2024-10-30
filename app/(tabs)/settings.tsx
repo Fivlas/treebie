ac84d1779642fea7f0e475d489e14a499d71d98c
TymianekPL <wilczew.tymon@gmail.com>
2024-10-30 19:52:55 +0100

    Make settings persistent

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,14 +1,15 @@
 import {SafeAreaView, Switch, Text, View, TouchableHighlight, Appearance} from 'react-native';
-
 import React, {useEffect, useState} from "react";
 import {ThemedText} from '@/components/ThemedText';
-
+import {THEME_PREFERENCE_KEY} from "@/hooks/useColorScheme";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function Settings() {
     const [darkTheme, changeDarkToggle] = useState<boolean>(false);
     const canChangeTheme = typeof (Appearance.setColorScheme) === "function";
     useEffect(() => {
         if (canChangeTheme) Appearance.setColorScheme(darkTheme ? "dark" : "light");
+        AsyncStorage.setItem(THEME_PREFERENCE_KEY, darkTheme ? "dark" : "light");
     }, [darkTheme]);
     return (
         <SafeAreaView className={"flex-1 p-12 mt-10 flex flex-col items-center"}>
