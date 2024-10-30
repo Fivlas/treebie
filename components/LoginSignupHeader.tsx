f9adde6107707750175f893e2e5aea4a581979d7
JASOJ <janniewq2006@gmail.com>
2024-10-30 19:23:41 +0100

    firebase
    firebase + firestore

--- components/LoginSignupHeader.tsx
+++ components/LoginSignupHeader.tsx
@@ -1,27 +1,34 @@
-﻿import { StyleSheet, View} from "react-native";
+﻿import { Pressable, StyleSheet, TextInput, View, Text} from "react-native";
 import {TitleHeader} from "@/components/TitleHeader";
 import {SubtitleHeader} from "@/components/SubtitleHeader";
 import React from "react";
 import {AnimatedLeaf} from "@/components/LoginSignup/AnimatedLeaf";
+import DismissKeyboard from "./DismissKeyboard";
 
 export default function LoginSignupHeader() {
     return (
-        <View style={styles.container}>
-            <View className={"relative items-center justify-center"}>
-                <View className={"absolute flex flex-row items-end"}>
-                    <View  style={{
-                        right: -40,
-                        top: 30
-                    }}><AnimatedLeaf className={"border-leafSecondary"}/></View>
-                    <View  style={{
-                        left: -40,
-                        top: 15
-                    }}><AnimatedLeaf className={"border-primary"}/></View>
+        <DismissKeyboard>
+            <View style={styles.container}>
+                <View className={"relative items-center justify-center"}>
+                    <View className={"absolute flex flex-row items-end"}>
+                        <View  style={{
+                            right: -40,
+                            top: 30
+                        }}><AnimatedLeaf className={"border-leafSecondary"}/></View>
+                        <View  style={{
+                            left: -40,
+                            top: 15
+                        }}><AnimatedLeaf className={"border-primary"}/></View>
+                    </View>
                 </View>
+                <TitleHeader>Podejmij Eko-wyzwanie!</TitleHeader>
+                <SubtitleHeader>Wykonuj zadania, zbieraj punkty i wymieniaj na nagrody</SubtitleHeader>
+                <TextInput placeholder="login" className="text-3xl text-white" />
+                <TextInput placeholder="haslo" className="text-3xl text-white" />
+                <Pressable><Text className="text-white">Submit</Text></Pressable>
             </View>
-            <TitleHeader>Podejmij Eko-wyzwanie!</TitleHeader>
-            <SubtitleHeader>Wykonuj zadania, zbieraj punkty i wymieniaj na nagrody</SubtitleHeader>
-        </View>
+        </DismissKeyboard>
+        
     );
 }
 
