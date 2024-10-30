1630efb214ece251e131f2daaf0ebd22404cd82f
JASOJ <janniewq2006@gmail.com>
2024-10-30 19:23:41 +0100

    firebase
    firebase + firestore

---
+++ firebase.config.tsx
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { Extrapolation } from "react-native-reanimated";
+
+const firebaseConfig = {
+    apiKey: "REMOVED_KEY",
+    authDomain: "REMOVED_URL",
+    projectId: "treebie-66127",
+    storageBucket: "REMOVED_BUCKET",
+    messagingSenderId: "REMOVED_ID",
+    appId: "1:REMOVED_ID:android:926e964ee0f867c82590c5"
+};
+
+export const FIREBASE_APP = initializeApp(firebaseConfig);
+export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
+export const FIREBASE_DB = getFirestore(FIREBASE_APP);
