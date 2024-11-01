c8c76b98eedeaa6f84783b2cebf5499aa6a903ec
jakub Lagierski <jakublag0987@gmail.com>
2024-11-01 17:37:34 +0100

    commit

--- components/SubtitleHeader.tsx
+++ components/SubtitleHeader.tsx
@@ -1,20 +1,28 @@
-﻿import {StyleSheet, View, Text} from 'react-native';
+﻿import { StyleSheet, View, Text } from "react-native";
 
 export type SubtitleHeaderOptions = {
-    children: string;
+  children: string;
 };
 
-export function SubtitleHeader({children, ...rest}: Readonly<SubtitleHeaderOptions>) {
-    return (<View style={styles.container}><Text style={styles.headerText}
-                                                 className={"text-secondary"} {...rest} >{children}</Text></View>);
+export function SubtitleHeader({
+  children,
+  ...rest
+}: Readonly<SubtitleHeaderOptions>) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.headerText} className={"text-secondary"} {...rest}>
+        {children}
+      </Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {},
-    headerText: {
-        fontSize: 14,
-        width: "100%",
-        overflow: 'hidden',
-        textAlign: "center"
-    }
+  container: {},
+  headerText: {
+    fontSize: 14,
+    width: "100%",
+    overflow: "hidden",
+    textAlign: "center",
+  },
 });
