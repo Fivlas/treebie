3677baf37c4aa6100844d46d45ad2cc2933c37c4
JASOJ <janniewq2006@gmail.com>
2024-11-02 21:14:23 +0100

    REJESTRACJAJA DODANA + QUESTY W FIRESTORE
    rejestracja

--- app/(auth)/signup.tsx
+++ app/(auth)/signup.tsx
@@ -5,21 +5,46 @@ import {CustomInput} from "@/components/elements/CustomInput";
 import CustomButton from "@/components/elements/CustomButton";
 import {router} from "expo-router";
 import {LoginSignupFooter} from "@/components/elements/LoginSignupFooter";
+import { createUserWithEmailAndPassword, sendEmailVerification } from 'firebase/auth';
+import { FIREBASE_AUTH } from '@/firebase.config';
+import { useState } from 'react';
 
 
 export default function Signup() {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [repassword, setRepassword] = useState("");
+
     const handleSignupPress = () => {
         router.push("/login")
     }
+
+    const signUp = async () => {
+        try {
+            if(password == repassword){
+                const response = await createUserWithEmailAndPassword(FIREBASE_AUTH, email, password);
+                await sendEmailVerification(response.user);
+                alert('Mail weryfikacyjny został wysłany na adres ' + email);
+            }else{
+                console.log("pass != repass");
+            }
+        }
+        catch(error: any){
+            console.error('Błąd podczas rejestracji użytkownika:', error.message);
+            alert('Błąd: ' + error.message);
+        }
+    }
+
     return (
         <SafeAreaView className={"flex-1"}>
             <LoginSignupHeader/>
             <View className={"flex flex-col p-12 flex-1 justify-between"}>
                 <View>
-                    <CustomInput type={"email"} placeholder={"E-mail"}/>
-                    <CustomInput type={"password"} placeholder={"Hasło"}/>
-                    <CustomInput type={"password"} placeholder={"Powtórz hasło"}/>
-                    <CustomButton title={"Zarejestruj"} buttonType={"primary"}/>
+                    <CustomInput type={"email"} onChange={val => setEmail(val)} val={email} placeholder={"E-mail"} />
+                    <CustomInput type={"password"} onChange={val => setPassword(val)} val={password} placeholder={"Hasło"}/>
+                    <CustomInput type={"password"} onChange={val => setRepassword(val)} val={repassword} placeholder={"Powtórz hasło"}/>
+                    <CustomButton handlePress={signUp} title={"Zarejestruj"} buttonType={"primary"}/>
                     <TouchableOpacity onPress={handleSignupPress} className="mt-4">
                         <Text className="text-text text-center">Posiadasz konto? Zaloguj się!</Text>
                     </TouchableOpacity>
