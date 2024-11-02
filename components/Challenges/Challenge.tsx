9e8a85947591c7370d9da944b1f08f2a0ab68f89
jakublagierski <149930978+jakubLag@users.noreply.github.com>
2024-11-02 22:10:42 +0100

    started working on cahllenges list

--- components/Challenges/Challenge.tsx
+++ components/Challenges/Challenge.tsx
@@ -1,5 +1,9 @@
-import { View } from "react-native";
-
-export const Challenge = () => {
-  return <View>Wyzywanie</View>;
+import { View, Text } from "react-native";
+import { ChallengeType } from "./ChallengesList";
+export const Challenge = (challenge: ChallengeType) => {
+  return <View className="border rounded-lg p-2">
+  <Text className="text-xl font-bold">{}</Text>
+  <Text></Text>
+</View>;
 };
+export default Challenge;
--- components/Challenges/SearchBar.tsx
+++ components/Challenges/SearchBar.tsx
@@ -1,6 +1,6 @@
 import { TextInput, Text, View, Pressable } from "react-native"
 const SearchBar = () => {
-    return <View>
+    return <View className="mb-2">
         <TextInput placeholder="Znajdź wyzwanie..." className="bg-white text-black text-xl border border-gray-300 rounded-lg pl-4 h-16 mb-2"/>
         <Pressable className="border h-16 mx-auto w-full flex justify-center items-center bg-text rounded-lg" >
             <Text className="text-2xl bg-text text-white ">Szukaj</Text>
