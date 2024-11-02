4f280a9e06a79d52cff1d0a8a74bb6628b57e9f6
jakublagierski <149930978+jakubLag@users.noreply.github.com>
2024-11-02 21:40:17 +0100

    searchbar done

--- app/(tabs)/challenges.tsx
+++ app/(tabs)/challenges.tsx
@@ -1,18 +1,14 @@
-import { SafeAreaView, Text } from "react-native";
+import { SafeAreaView, Text, View } from "react-native";
 import SearchBar from "../../components/Challenges/SearchBar";
+import ChallengesList from "@/components/Challenges/ChallengesList";
 export default function Challenges() {
-    const data = [
-      {
-        title: "Uzbieraj 3 śmieci",
-        description: "Znajdź 3 odpady w twojej okolicy, które nie powinny się tam znaleźć i wyrzuć je w odpowiednie miejsce.",
-        difficultyLevel: 1,
-        difficultyName: "Łatwy",
-        pointsToGain: 50
-      } 
-    ];
   return (
     <SafeAreaView>
-      <SearchBar />
+      <View className="p-4">
+        <Text className="text-4xl font-bold mb-2">Wyzwania</Text>
+        <SearchBar />
+        <ChallengesList />
+      </View>
     </SafeAreaView>
   );
 }
