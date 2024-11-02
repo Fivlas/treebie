b7fdd3215598e6d5db6c13461f122e78ba7f8325
jakublagierski <149930978+jakubLag@users.noreply.github.com>
2024-11-02 21:40:17 +0100

    searchbar done

---
+++ components/Challenges/ChallengesList.tsx
@@ -0,0 +1,103 @@
+import { View, Text } from "react-native";
+
+const ChallengesList = (props: any) => {
+  const challengesData = [
+    {
+      title: "Zbieraj 5 puszek",
+      description:
+        "Znajdź i zbierz 5 puszek w najbliższym parku lub lesie i wrzuć je do pojemnika na aluminium.",
+      difficultyLevel: 1,
+      difficultyName: "łatwy",
+      pointsToGain: 30,
+      challengeGroup: "Zbieranie",
+    },
+    {
+      title: "Posadź kwiaty",
+      description:
+        "Kup sadzonki kwiatów i posadź je w swojej okolicy, aby upiększyć środowisko.",
+      difficultyLevel: 2,
+      difficultyName: "średni",
+      pointsToGain: 60,
+      challengeGroup: "Sadzenie",
+    },
+    {
+      title: "Oszczędzaj energię w domu",
+      description:
+        "Przez 3 dni wyłączaj światło w każdym pomieszczeniu, gdy wychodzisz. Pomóż zmniejszyć zużycie energii!",
+      difficultyLevel: 1,
+      difficultyName: "łatwy",
+      pointsToGain: 30,
+      challengeGroup: "Energia",
+    },
+    {
+      title: "Oszczędzanie wody",
+      description:
+        "Podczas mycia zębów zakręć kran, aby nie marnować wody. Powtarzaj przez tydzień.",
+      difficultyLevel: 1,
+      difficultyName: "łatwy",
+      pointsToGain: 30,
+      challengeGroup: "Zmiana nawyków",
+    },
+    {
+      title: "Nie używaj samochodu przez 3 dni",
+      description:
+        "Przez 3 dni korzystaj z komunikacji miejskiej lub roweru zamiast samochodu.",
+      difficultyLevel: 2,
+      difficultyName: "średni",
+      pointsToGain: 60,
+      challengeGroup: "Transport",
+    },
+    {
+      title: "Rozmowa o ekologii",
+      description:
+        "Porozmawiaj z kimś o znaczeniu ekologii i wymieńcie się wskazówkami, jak oszczędzać zasoby.",
+      difficultyLevel: 3,
+      difficultyName: "trudny",
+      pointsToGain: 100,
+      challengeGroup: "Interakcje",
+    },
+    {
+      title: "Kup produkty bez plastiku",
+      description:
+        "Przez 3 dni wybieraj tylko produkty, które nie są opakowane w plastik.",
+      difficultyLevel: 2,
+      difficultyName: "średni",
+      pointsToGain: 60,
+      challengeGroup: "Zakupy",
+    },
+    {
+      title: "Sprzątanie plaży",
+      description:
+        "Wybierz się na plażę i zbierz śmieci, które znajdziesz na brzegu.",
+      difficultyLevel: 3,
+      difficultyName: "trudny",
+      pointsToGain: 100,
+      challengeGroup: "Zbieranie",
+    },
+    {
+      title: "Posadź drzewko",
+      description:
+        "Kup sadzonkę drzewa i posadź ją w swojej okolicy, aby pomóc środowisku.",
+      difficultyLevel: 2,
+      difficultyName: "średni",
+      pointsToGain: 60,
+      challengeGroup: "Sadzenie",
+    },
+    {
+      title: "Zminimalizuj odpady",
+      description:
+        "Przez tydzień unikaj kupowania produktów w jednorazowych opakowaniach.",
+      difficultyLevel: 3,
+      difficultyName: "trudny",
+      pointsToGain: 100,
+      challengeGroup: "Zmiana nawyków",
+    },
+  ];
+  
+  return (
+    <View>
+      
+    </View>
+  );
+};
+export default ChallengesList;
