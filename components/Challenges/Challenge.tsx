7f60284048c08e5f4bcd575b5bbb391d53d8b6f6
jakublagierski <149930978+jakubLag@users.noreply.github.com>
2024-11-03 12:27:31 +0100

    challenges card styling

--- components/Challenges/Challenge.tsx
+++ components/Challenges/Challenge.tsx
@@ -1,9 +1,30 @@
 import { View, Text } from "react-native";
 import { ChallengeType } from "./ChallengesList";
-export const Challenge = (challenge: ChallengeType) => {
-  return <View className="border rounded-lg p-2">
-  <Text className="text-xl font-bold">{}</Text>
-  <Text></Text>
-</View>;
+export const Challenge = (props: ChallengeType) => {
+  const challenge = props;
+  const newDesc = challenge.description;
+  const fixedDifficultyName = challenge.difficultyName[0].toUpperCase() + challenge.difficultyName.slice(1, 10000);
+  const getColor = () => {
+    if(challenge.difficultyLevel === 1) {
+      return 'text-primary';
+    }
+    else if(challenge.difficultyLevel === 2) {
+      return 'text-orange-600';
+    }
+    else if (challenge.difficultyLevel === 3) {
+      return 'text-red-600';
+    }
+    else {
+      return "error with color"
+    }
+  }
+  return (
+    <View className=" rounded-lg p-2 h-72 bg-background shadow ">
+      <Text className="text-3xl font-bold">{challenge.title}</Text>
+      <Text className="text-tint border w-72 text-primary">{newDesc.slice(0, 40)}...</Text>
+      <Text>Stopień trudności</Text>
+      <Text className={"text-3xl " + getColor()}>{fixedDifficultyName}</Text>
+    </View>
+  );
 };
 export default Challenge;
