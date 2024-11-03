0f95cd6e2912e6f332bfafe23f8e304bf297be71
jakublagierski <149930978+jakubLag@users.noreply.github.com>
2024-11-03 19:39:11 +0100

    rendering single tips details

--- app/tip/[id]/index.tsx
+++ app/tip/[id]/index.tsx
@@ -1,16 +1,38 @@
 import CustomButton from "@/components/elements/CustomButton";
 import { router, useLocalSearchParams } from "expo-router";
 import { SafeAreaView, Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { doc, getDoc } from "firebase/firestore";
+import { FIREBASE_DB } from "@/firebase.config";
 
 const index = () => {
-    const local = useLocalSearchParams();
+  const [tip, setTip] = useState<object | any>();
+  const local = useLocalSearchParams();
 
-    return (
-        <SafeAreaView>
-            <Text>{local.id}</Text>
-            <CustomButton title="Back" handlePress={() => router.push("/(tabs)/")} buttonType="primary"/>
-        </SafeAreaView>
-    );
+  useEffect(() => {
+    const getData = async (id: string) => {
+      try {
+        const docRef = doc(FIREBASE_DB, "tips", id);
+        const res = await getDoc(docRef);
+        setTip(res.data())
+        
+      } catch (error) {
+        console.error("Error fetching document: ", error);
+      }
+    };
+    getData(local.id.toString());
+  }, []);
+  return (
+    <SafeAreaView>
+      <Text className="text-2xl">{tip?.title}</Text>
+      <Text>{local.id}</Text>
+      <CustomButton
+        title="Back"
+        handlePress={() => router.push("/(tabs)/")}
+        buttonType="primary"
+      />
+    </SafeAreaView>
+  );
 };
 
 export default index;
