e420c4182125a5aa21216e1d2e118f24f53aa2ad
jakublagierski <149930978+jakubLag@users.noreply.github.com>
2024-11-03 22:34:30 +0100

    list of challenges styling

--- components/Challenges/Challenge.tsx
+++ components/Challenges/Challenge.tsx
@@ -4,26 +4,20 @@ export const Challenge = (props: ChallengeType) => {
   const challenge = props;
   const newDesc = challenge.description;
   const fixedDifficultyName = challenge.difficultyName[0].toUpperCase() + challenge.difficultyName.slice(1, 10000);
-  const getColor = () => {
-    if(challenge.difficultyLevel === 1) {
-      return 'text-primary';
-    }
-    else if(challenge.difficultyLevel === 2) {
-      return 'text-orange-600';
-    }
-    else if (challenge.difficultyLevel === 3) {
-      return 'text-red-600';
-    }
-    else {
-      return "error with color"
-    }
-  }
+  
   return (
-    <View className=" rounded-lg p-2 h-72 bg-background shadow ">
-      <Text className="text-3xl font-bold">{challenge.title}</Text>
-      <Text className="text-tint border w-72 text-primary">{newDesc.slice(0, 40)}...</Text>
+    <View className="rounded-lg h-40 bg-background shadow flex flex-row overflow-hidden">
+      {/* <Text className="text-3xl font-bold">{challenge.title}</Text>
+      <Text className="w-full text-gray-300">{newDesc.slice(0, 10000)}</Text>
       <Text>Stopień trudności</Text>
-      <Text className={"text-3xl " + getColor()}>{fixedDifficultyName}</Text>
+      <Text className={"text-3xl " + getColor()}>{fixedDifficultyName}</Text> */}
+      <View className="w-2/3">
+      <Text className="text-2xl font-bold p-2">{challenge.title}</Text>
+
+      </View>
+      <View className="w-1/3 flex justify-center items-center bg-primary">
+        <Text className="text-secondary text-6xl">{challenge.pointsToGain}</Text>
+      </View>
     </View>
   );
 };
