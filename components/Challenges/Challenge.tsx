8532fc1ed4f1854a3fbc59954956c9251a428b0f
jakublagierski <149930978+jakubLag@users.noreply.github.com>
2024-11-05 19:53:19 +0100

    mapping challenges

--- components/Challenges/Challenge.tsx
+++ components/Challenges/Challenge.tsx
@@ -1,22 +1,28 @@
 import { View, Text } from "react-native";
 import { ChallengeType } from "./ChallengesList";
-export const Challenge = (props: ChallengeType) => {
-  const challenge = props;
-  const newDesc = challenge.description;
-  const fixedDifficultyName = challenge.difficultyName[0].toUpperCase() + challenge.difficultyName.slice(1, 10000);
+export const Challenge = ({title, description, difficultyLevel, pointsToGain, challengeGroup, difficultyName} : ChallengeType) => {
+  // const challenge = props;
   
+  const fixedDifficultyName = difficultyName[0].toUpperCase() + difficultyName.slice(1, 10000);
+  const getColor = () => {
+    const difficulty = difficultyLevel;
+    if(difficulty === 1) return "primary";
+    else if(difficulty === 2) return "orange";
+    else if(difficulty === 3) return "red";
+    else return "error with colors";
+  }
   return (
-    <View className="rounded-lg h-40 bg-background shadow flex flex-row overflow-hidden">
-      {/* <Text className="text-3xl font-bold">{challenge.title}</Text>
-      <Text className="w-full text-gray-300">{newDesc.slice(0, 10000)}</Text>
-      <Text>Stopień trudności</Text>
-      <Text className={"text-3xl " + getColor()}>{fixedDifficultyName}</Text> */}
-      <View className="w-2/3">
-      <Text className="text-2xl font-bold p-2">{challenge.title}</Text>
-
+    <View className="h-52 my-2 rounded-lg bg-background shadow flex flex-row overflow-hidden">
+      
+      <View className="w-2/3 p-3">
+      <Text className="text-2xl font-bold">{title}</Text>
+      <Text className="text-base">{description}</Text>
+      <Text className={`text-primary text-3xl text-${getColor()}`}>{fixedDifficultyName}</Text>
+      <Text className="text-right text-gray-300">#{challengeGroup} #eko</Text>
       </View>
-      <View className="w-1/3 flex justify-center items-center bg-primary">
-        <Text className="text-secondary text-6xl">{challenge.pointsToGain}</Text>
+      <View className={`w-1/3 flex justify-center items-center bg-${getColor()}`}>
+        <Text className="text-secondary text-6xl">{pointsToGain}</Text>
+        <Text className="text-secondary text-2xl">punktów</Text>
       </View>
     </View>
   );
