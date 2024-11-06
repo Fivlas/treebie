5c21e3c58bc4dc9de2dacfe9f8971589d3ffd782
jakublagierski <149930978+jakubLag@users.noreply.github.com>
2024-11-06 22:38:36 +0100

    working on dynamic chall route

--- components/Challenges/Challenge.tsx
+++ components/Challenges/Challenge.tsx
@@ -9,20 +9,14 @@ const Challenge = ({
   pointsToGain,
   challengeGroup,
   difficultyName,
-  color
+  color,
 }: ChallengeType) => {
-
   const fixedDifficultyName =
-    difficultyName[0].toUpperCase() + difficultyName.slice(1, 10000);
-  const getColor = () => {
-    if (difficultyLevel === 1) return "primary";
-    if (difficultyLevel === 2) return "orange";
-    if (difficultyLevel === 3) return "red";
-    else console.log("Color error");
-  };
+    difficultyName[0].toUpperCase() + difficultyName.slice(1);
+
   return (
     <TouchableOpacity onPress={() => router.push(`/challenge/${id}`)}>
-      <View  className="my-2 rounded-lg bg-background shadow flex flex-row overflow-hidden">
+      <View className="my-2 rounded-lg bg-background shadow flex flex-row overflow-hidden">
         <View className="w-2/3 p-3">
           <Text className="text-2xl font-bold">{title}</Text>
           <Text className="text-base">{description}</Text>
@@ -33,9 +27,7 @@ const Challenge = ({
             #{challengeGroup} #eko
           </Text>
         </View>
-        <View
-          className={`w-1/3 flex justify-center items-center ${color?.bg}`}
-        >
+        <View className={`w-1/3 flex justify-center items-center ${color?.bg}`}>
           <Text className="text-secondary text-6xl">{pointsToGain}</Text>
           <Text className="text-secondary text-2xl">punktów</Text>
         </View>
