f754e2122d9d3f1aa171e5306326f772a25b9bf1
Fivlas <filip.skoczylas777007@gmail.com>
2024-11-07 20:31:35 +0100

    adds simple tree

--- app/(tabs)/tree.tsx
+++ app/(tabs)/tree.tsx
@@ -1,6 +1,7 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
 import { Feather } from '@expo/vector-icons'
 import React from 'react'
-import { SafeAreaView, Text, TouchableOpacity, View } from 'react-native'
+import { Image, SafeAreaView, Text, TouchableOpacity, View } from 'react-native'
 
 const tree = () => {
     return (
@@ -9,10 +10,14 @@ const tree = () => {
                 <TouchableOpacity>
                     <Feather name="shopping-bag" size={24} color="black" />
                 </TouchableOpacity>
-                
+
                 <TouchableOpacity>
                     <Feather name="camera" size={24} color="black" />
                 </TouchableOpacity>
+            </View>
+            <View className='border border-red-700 h-full px-8 mt-4'>
+                <Image source={require('@/assets/images/tree15.png')} className='h-[510px] w-[300px] mx-auto' />
+                <Text>progrtess</Text>
             </View>
         </SafeAreaView>
     )
