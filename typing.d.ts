92d18632ec27ea6f1185a30740ffd129d7fcd470
jakublagierski <149930978+jakubLag@users.noreply.github.com>
2024-11-07 19:55:56 +0100

    stash

--- typing.d.ts
+++ typing.d.ts
@@ -11,8 +11,19 @@ interface ChallengeData {
     challengeGroup: string;
     difficultyName: string;
 }
+interface ChallengeData {
+    title: string;
+    description: string;
+    difficultyLevel: number;
+    pointsToGain: number;
+    challengeGroup: string;
+    difficultyName: string;
+}
 
 interface TipFields extends TipData {
+    id: string;
+}
+interface ChallengeFields extends ChallengeData {
     id: string;
 }
 
