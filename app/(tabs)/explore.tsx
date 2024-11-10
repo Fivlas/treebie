c99e2ffce347fe853eb5d65d2ac19d81f259f7fe
Fivlas <filip.skoczylas777007@gmail.com>
2024-11-10 23:53:09 +0100

    adds shop small ui changes in challenges

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -4,29 +4,127 @@ import { FIREBASE_DB } from "@/firebase.config";
 import { doc, getDoc } from "firebase/firestore";
 import { useState, useEffect } from "react";
 import CustomButton from "@/components/elements/CustomButton";
+import { StyleSheet } from "react-native";
 
 const index = () => {
-  const [challenge, setChallenge] = useState<object | any>();
-  const local = useLocalSearchParams();
+    const [challenge, setChallenge] = useState<object | any>();
+    const local = useLocalSearchParams();
+    const [tempData, setTempData] = useState<[string, string, string]>([
+        "#606c38",
+        "#cc7a00",
+        "#a23b3b",
+    ]);
+    type actionType = "start" | "ongoing" | "end" | any;
+    const [actualAction, setActualAction] = useState<actionType>("start");
+    // const actionData = [
+    //   {
+    //     name: "start",
+    //     color: tempData[0],
+    //   },
+    //   {
+    //     name: "ongoing",
+    //     color: tempData[1],
+    //   },
+    //   {
+    //     name: "end",
+    //     color: tempData[2],
+    //   },
+    // ];
+    // useEffect(() => {
+    //   const getData = async (id: string) => {
+    //     try {
+    //       const docRef = doc(FIREBASE_DB, "quests", id);
+    //       const res = await getDoc(docRef);
+    //       setChallenge(res.data());
+    //     } catch (error) {
+    //       console.error("Error fetching document: ", error);
+    //     }
+    //   };
+    //   getData(local.id.toString());
+    // }, []);
+    type ActionDataType = {
+        color: string;
+        statusText: string;
+        buttonText: string;
+    }
+    console.log(actualAction);
+    const getDataByAction = (): ActionDataType => {
+        if (actualAction === "start") {
+            return {
+                color: "primary",
+                statusText: "Dostępne",
+                buttonText: "Rozpocznij",
+            };
+        } else if (actualAction === "ongoing") {
+            return {
+                color: "orange",
+                statusText: "W trakcie",
+                buttonText: "Zakończ",
+            };
+        } else if (actualAction === "end") {
+            return {
+                color: "red",
+                statusText: "Zakończone",
+                buttonText: "Zakończono",
+            };
+        }
+        return {
+            color: "red",
+            statusText: "Error",
+            buttonText: "Error",
+        };
+    };
+    return (
+        <SafeAreaView>
+            <View className="p-4 h-full">
+                <View className="p-2 rounded-lg bg-background flex flex-column justify-between h-full">
+                    <View className="flex flex-column gap-4">
+                        <Text className="text-4xl font-bold">
+                            {challenge?.title}Posadź drzewka
+                        </Text>
+                        <Text className="text-gray-400 text-xl">
+                            Kup sadzonkę drzewa i posadź ją w swojej okolicy, aby pomóc
+                            środowisku.
+                        </Text>
+                        <Text
+                            className={`text-3xl text-${getDataByAction()?.color} font-bold`}
+                        >
+                            {getDataByAction()?.statusText}
+                        </Text>
+                    </View>
 
-  // useEffect(() => {
-  //   const getData = async (id: string) => {
-  //     try {
-  //       const docRef = doc(FIREBASE_DB, "quests", id);
-  //       const res = await getDoc(docRef);
-  //       setChallenge(res.data());
-  //     } catch (error) {
-  //       console.error("Error fetching document: ", error);
-  //     }
-  //   };
-  //   getData(local.id.toString());
-  // }, []);
-
-  return (
-    <SafeAreaView>
-      <Text className="text-4xl font-bold">{challenge?.title}To jest tytuł</Text>
-      <Text></Text>
-    </SafeAreaView>
-  );
+                    <View className="w-full flex items-center">
+                        <View
+                            className={`w-[300px] h-[300px] rounded-full bg-${getDataByAction()?.color
+                                } flex justify-center items-center`}
+                        >
+                            <View className="w-[240px] h-[240px] rounded-full bg-white flex justify-center items-center">
+                                <Text
+                                    className={`text-[50px] text-${getDataByAction()?.color}`}
+                                >
+                                    50 pkt
+                                </Text>
+                            </View>
+                        </View>
+                    </View>
+                    <CustomButton
+                        title={getDataByAction()?.buttonText.toString()}
+                        buttonType="primary"
+                        textStyles="text-3xl"
+                        handlePress={() => {
+                            setActualAction(
+                                actualAction === "start"
+                                    ? "ongoing"
+                                    : actualAction === "ongoing"
+                                        ? "end"
+                                        : actualAction
+                            );
+                        }}
+                    />
+                </View>
+            </View>
+        </SafeAreaView>
+    );
 };
+
 export default index;
--- app/(tabs)/tree.tsx
+++ app/(tabs)/tree.tsx
@@ -1,5 +1,7 @@
 /* eslint-disable @typescript-eslint/no-require-imports */
+import { ThemedText } from '@/components/ThemedText';
 import { Feather } from '@expo/vector-icons';
+import { Href, router } from 'expo-router';
 import React, { useState } from 'react';
 import { Image, SafeAreaView, Text, TouchableOpacity, View } from 'react-native';
 //@ts-ignore
@@ -11,12 +13,14 @@ const Tree = () => {
     return (
         <SafeAreaView className="flex-1">
             {/* Top Navigation Icons */}
-            <View className="flex flex-row justify-between px-8 mt-4">
-                <TouchableOpacity>
+            <View className="flex flex-row justify-between px-8 mt-2 items-center">
+                <ThemedText
+                    className="text-4xl font-semibold mb-2"
+                >
+                    Twoje drzewo
+                </ThemedText>
+                <TouchableOpacity onPress={() => router.push("/shop?redirect=tree" as Href)}>
                     <Feather name="shopping-bag" size={24} color="black" />
-                </TouchableOpacity>
-                <TouchableOpacity>
-                    <Feather name="camera" size={24} color="black" />
                 </TouchableOpacity>
             </View>
 
@@ -25,14 +29,14 @@ const Tree = () => {
                 <Image
                     source={require('@/assets/images/tree15.png')}
                     className="h-[510px] w-[300px] mx-auto"
-                    // className="h-[595px] w-[300px] mx-auto"
+                // className="h-[595px] w-[300px] mx-auto"
                 />
-                    <View className='absolute bottom-32 left-7'>
-                        <View className='flex-row items-center gap-2'>
-                            <Text className='font-bold'>Poziom: 3 </Text>
-                            <ProgressBar progress={0.5} width={250} height={20} color="#606c38" animating borderWidth={2} className="rounded-full"/>
-                        </View>
+                <View className='absolute bottom-32 left-7'>
+                    <View className='flex-row items-center gap-2'>
+                        <ThemedText className='font-bold'>Poziom: 3 </ThemedText>
+                        <ProgressBar progress={0.5} width={250} height={20} color="#606c38" animating borderWidth={2} className="rounded-full" />
                     </View>
+                </View>
             </View>
         </SafeAreaView>
     );
