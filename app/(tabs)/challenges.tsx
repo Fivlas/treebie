1383fb00d8b0b2d0092227bda073e4be4d2161b7
Fivlas <filip.skoczylas777007@gmail.com>
2024-11-15 22:03:26 +0100

    fixes challange view

--- app/(tabs)/challenges.tsx
+++ app/(tabs)/challenges.tsx
@@ -4,7 +4,7 @@ import {
   Platform,
   SafeAreaView,
   View,
-  ActivityIndicator,
+  Text,
 } from "react-native";
 import SearchBar from "../../components/Challenges/SearchBar";
 import ChallengesList from "@/components/Challenges/ChallengesList";
@@ -14,9 +14,8 @@ import { ThemedText } from "@/components/ThemedText";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { doc, getDoc } from "firebase/firestore";
 import { useUser } from "@/hooks/useUser";
-import { FIREBASE_DB } from "@/functions/firebaseConfig";
 import Challenge from "@/components/Challenges/Challenge";
-
+import { FIREBASE_DB } from "@/functions/firebaseConfig";
 interface ParentProps {}
 type Quest = {
   challengeGroup: string;
@@ -26,15 +25,16 @@ type Quest = {
   pointsToGain: number;
   title: string;
 };
-
 const Challenges: React.FC<ParentProps> = () => {
   const { user, loading } = useUser();
   const [currentQuestId, setCurrentQuestId] = useState<string | undefined>("");
-  const [currentQuest, setCurrentQuest] = useState<Quest | undefined>();
+  const [currentQuest, setCurrentQuest] = useState<Quest>();
   const [query, setQuery] = useState<string>("");
-  const [isQuestLoading, setIsQuestLoading] = useState<boolean>(true);
-
+  const getQuery = (q: string) => {
+    setQuery(q);
+  };
   const backgroundColor = useThemeColor({ light: "", dark: "" }, "background");
+  const textColor = useThemeColor({ light: "", dark: "" }, "text");
 
   const closeApp = () => {
     if (Platform.OS === "android") {
@@ -42,118 +42,104 @@ const Challenges: React.FC<ParentProps> = () => {
     }
   };
 
-  const getQuery = (q: string) => {
-    setQuery(q);
-  };
-
   useEffect(() => {
-    const fetchCurrentQuest = async () => {
-      setIsQuestLoading(true);
+    if (!user || loading) return;
+
+    const checkFirstUse = async () => {
       try {
-        if (!user) return;
-        const userDocRef = doc(FIREBASE_DB, "users", user.uid);
-        const userDoc = await getDoc(userDocRef);
-  
-        if (!userDoc.exists()) {
-          console.warn("User document does not exist.");
-          setCurrentQuest(undefined);
-          setIsQuestLoading(false);
-          return;
-        }
-  
-        const userData = userDoc.data();
-        const questId = userData?.currentQuest;
-        if (!questId) {
-          console.log("No current quest found for this user.");
-          setCurrentQuest(undefined);
-          setIsQuestLoading(false);
-          return;
-        }
-  
-        const questDocRef = doc(FIREBASE_DB, "quests", questId);
-        const questDoc = await getDoc(questDocRef);
-  
-        if (questDoc.exists()) {
-          const questData = questDoc.data() as Quest;
-          setCurrentQuest(questData);
-          setCurrentQuestId(questId);
-        } else {
-          console.error("Quest document does not exist.");
-          setCurrentQuest(undefined);
+        const hasSeenAlert = await AsyncStorage.getItem("hasSeenAlert");
+
+        if (!hasSeenAlert) {
+          askAlert();
         }
       } catch (error) {
-        console.error("Error fetching current quest:", error);
-        setCurrentQuest(undefined);
-      } finally {
-        setIsQuestLoading(false);
+        console.log("Error checking or setting the alert flag:", error);
       }
     };
-  
+
+    const askAlert = () => {
+      Alert.alert(
+        "Uwaga",
+        "Czy zobowiązujesz się do rzetelnego wypełniania wyzwań?",
+        [
+          {
+            text: "Nie",
+            onPress: () => closeAppAlert(),
+            style: "destructive",
+          },
+          {
+            text: "Tak",
+            onPress: () => AsyncStorage.setItem("hasSeenAlert", "true"),
+          },
+        ]
+      );
+    };
+    const closeAppAlert = () => {
+      Alert.alert(
+        "Kondolencje",
+        "Bardzo nam przykro, że nie potrafisz być uczciwy. Przemyśl swój wybór",
+        [
+          {
+            text: "Zamknij",
+            onPress: () => closeApp(),
+            style: "destructive",
+          },
+        ]
+      );
+    };
+    // const getCurrentQuestId = async () => {
+    //   try {
+    //     const currentQuestIdRef = doc(FIREBASE_DB, "users", user.uid);
+    //     const questDoc = await getDoc(currentQuestIdRef);
+
+    //     if (questDoc.exists()) {
+    //       const userData = questDoc.data();
+    //       setCurrentQuestId(userData?.currentQuest || null);
+    //     }
+    //   } catch (e) {
+    //     console.log(e);
+    //   }
+    // };
+
+    const fetchCurrentQuest = async () => {
+      try {
+        // Krok 1: Pobierz currentQuest z kolekcji users
+        const userDocRef = doc(FIREBASE_DB, "users", user.uid);
+        const userDoc = await getDoc(userDocRef);
+
+        if (userDoc.exists()) {
+          const userData = userDoc.data();
+          setCurrentQuestId(userData?.currentQuest);
+
+          if (currentQuestId) {
+            // Krok 2: Użyj currentQuest do pobrania odpowiedniego dokumentu w quests
+            const questDocRef = doc(FIREBASE_DB, "quests", currentQuestId);
+            const questDoc = await getDoc(questDocRef);
+
+            if (questDoc.exists()) {
+              setCurrentQuest(questDoc.data() as Quest);
+            } else {
+              console.log("Dokument quest nie istnieje.");
+            }
+          } else {
+            console.log("Brak currentQuest w dokumencie użytkownika.");
+          }
+        } else {
+          console.log("Dokument użytkownika nie istnieje.");
+        }
+      } catch (error) {
+        console.error("Błąd podczas pobierania danych:", error);
+      }
+    };
+
+    checkFirstUse();
     fetchCurrentQuest();
-  }, []); // Empty dependency array ensures this runs every render
-
-  // Fetch the current quest from Firestore
-  const fetchCurrentQuest = async () => {
-    setIsQuestLoading(true);
-    try {
-      if (!user) return;
-      const userDocRef = doc(FIREBASE_DB, "users", user.uid);
-      const userDoc = await getDoc(userDocRef);
-
-      if (!userDoc.exists()) {
-        console.warn("User document does not exist.");
-        setCurrentQuest(undefined);
-        setIsQuestLoading(false);
-        return;
-      }
-
-      const userData = userDoc.data();
-      const questId = userData?.currentQuest;
-      if (!questId) {
-        console.log("No current quest found for this user.");
-        setCurrentQuest(undefined);
-        setIsQuestLoading(false);
-        return;
-      }
-
-      const questDocRef = doc(FIREBASE_DB, "quests", questId);
-      const questDoc = await getDoc(questDocRef);
-
-      if (questDoc.exists()) {
-        const questData = questDoc.data() as Quest;
-        setCurrentQuest(questData);
-        setCurrentQuestId(questId);
-      } else {
-        console.error("Quest document does not exist.");
-        setCurrentQuest(undefined);
-      }
-    } catch (error) {
-      console.error("Error fetching current quest:", error);
-      setCurrentQuest(undefined);
-    } finally {
-      setIsQuestLoading(false);
-    }
-  };
-
-  // Replace useFocusEffect with useEffect
-  useEffect(() => {
-    if (user && !loading) {
-      fetchCurrentQuest();
-    }
-  }, [user, loading]); // Re-fetch current quest whenever user or loading changes
-
+  }, [user, loading, currentQuestId]);
   const getColor = (level: number) => {
-    switch (level) {
-      case 1:
-        return { text: "text-primary", bg: "bg-primary" };
-      case 2:
-        return { text: "text-orange", bg: "bg-orange" };
-      case 3:
-        return { text: "text-red", bg: "bg-red" };
-      default:
-        console.log("Invalid difficulty level");
-        return { text: "text-gray", bg: "bg-gray" };
-    }
+    if (level === 1) return { text: "text-primary", bg: "bg-primary" };
+    if (level === 2) return { text: "text-orange", bg: "bg-orange" };
+    if (level === 3) return { text: "text-red", bg: "bg-red" };
+    else console.log("Color error");
   };
 
   return (
@@ -163,25 +149,23 @@ const Challenges: React.FC<ParentProps> = () => {
           Wyzwania
         </ThemedText>
       </View>
-
       <SearchBar getData={getQuery} />
-
-      {/* Display loading indicator or current quest */}
-      {isQuestLoading ? (
-        <ActivityIndicator size="large" color="#798156" />
-      ) : currentQuest && (
+      {currentQuestId ? (
         <View className="px-4">
-          <ThemedText className="text-2xl font-semibold">Aktywne zadanie</ThemedText>
-          <Challenge
-            id={currentQuestId!}
-            title={currentQuest.title}
-            difficultyLevel={currentQuest.difficultyLevel}
-            color={getColor(currentQuest.difficultyLevel)}
-            asActive={true}
-          />
+          <Text className="text-2xl">Aktywne zadanie</Text>
+          {currentQuest && (
+            <Challenge
+              id={currentQuestId}
+              title={currentQuest.title}
+              difficultyLevel={currentQuest.difficultyLevel}
+              color={getColor(currentQuest.difficultyLevel)}
+              asActive={true}
+            />
+          )}
         </View>
+      ) : (
+        null 
       )}
-
       <ChallengesList queryToFilter={query} />
     </SafeAreaView>
   );
